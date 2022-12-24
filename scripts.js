e4a3401852f0f2d638a21265282af6edae0d428b
Vlad Solovey <connectwithvlad@gmail.com>
2022-12-25 02:26:09 +0300

    feat: add display books functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+// Interface elements
+
+const addButtonMain = document.querySelector('#add-book-main');
+const modalWindow = document.querySelector('#modal');
+const closeModalButton = document.querySelector('#close-modal-button');
+const addBookForm = document.querySelector('#add-book-form');
+const libraryContainer = document.querySelector('#library');
+
 // Data structures
 
 function Library(books = []) {
@@ -12,6 +20,15 @@ Library.prototype.remove = function removeBookFromLibrary(index) {
   this.books.splice(index, 1);
 };
 
+Library.prototype.displayBook = function displayBookOnPage(book, index) {
+  const bookDisplayable = createDisplayableBook(book, index);
+  libraryContainer.appendChild(bookDisplayable);
+};
+
+Library.prototype.displayAll = function displayAllBooksOnPage() {
+  this.books.forEach((book, index) => this.displayBook(book, index));
+};
+
 function Book(title, author, numberOfPages, isRead) {
   this.title = title;
   this.author = author;
@@ -24,11 +41,6 @@ Book.prototype.info = function getBookInfo() {
 };
 
 // Interface interactions
-
-const addButtonMain = document.querySelector('#add-book-main');
-const modalWindow = document.querySelector('#modal');
-const closeModalButton = document.querySelector('#close-modal-button');
-const addBookForm = document.querySelector('#add-book-form');
 
 const library = new Library();
 
@@ -51,10 +63,45 @@ function createBookFromData(bookData) {
   return new Book(bookData.title, bookData.author, bookData.numberOfPages, bookData.isRead);
 }
 
+function createDisplayableBook(book, index) {
+  const bookDisplayable = document.createElement('div');
+  const bookTitle = document.createElement('h2');
+  const bookAuthor = document.createElement('h3');
+  const bookPagesNumber = document.createElement('p');
+  const bookState = document.createElement('p');
+
+  // for connection between library array and books on page
+  bookDisplayable.setAttribute('data-index', index);
+
+  bookDisplayable.classList.add('book-card');
+  bookTitle.classList.add('book-title');
+  bookAuthor.classList.add('book-author');
+  bookPagesNumber.classList.add('book-pages-number');
+  bookState.classList.add('book-state');
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookPagesNumber.textContent = book.numberOfPages;
+  bookState.textContent = book.isRead ? 'read' : 'not read';
+
+  bookDisplayable.appendChild(bookTitle);
+  bookDisplayable.appendChild(bookAuthor);
+  bookDisplayable.appendChild(bookPagesNumber);
+  bookDisplayable.appendChild(bookState);
+
+  return bookDisplayable;
+}
+
+function clearDisplayedBooks() {
+  libraryContainer.innerHTML = '';
+}
+
 function handleBookInput(event) {
   const bookData = getDataFromInput();
   const book = createBookFromData(bookData);
   library.add(book);
+  clearDisplayedBooks();
+  library.displayAll();
 
   changeModalVisibility();
   event.preventDefault();
